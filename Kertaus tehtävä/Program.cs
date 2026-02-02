e48538f58c8156ec965050e4601b7262434b317d
akuuu <s2401258@edu.bc.fi>
2026-02-02 10:01:49 +0200

    tehtäviä

--- Kertaus tehtävä/Program.cs
+++ Kertaus tehtävä/Program.cs
@@ -1,75 +1,76 @@
 ﻿using System;
 
-namespace Kertaus_tehtävä
+class Program
 {
-    internal class Program
+    static Random random = new Random();
+
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.WriteLine("Tervetuloa ritari peliin!");
+        Peli();
+    }
+
+    static void Peli()
+    {
+        int ritari = 20;
+        int örkki = 20;
+
+        while (ritari > 0 && örkki > 0)
         {
-            Random random = new Random();
-
-            Console.WriteLine("Tervetuloa ritari peliin!");
-
-            int RitariHitPoints = 20;
-            int ÖrkkiHitPoints = 20;
-
-            while (RitariHitPoints > 0 && ÖrkkiHitPoints > 0)
-            {
-                Console.WriteLine("----------------------------");
-                Console.WriteLine("Ritari HP: " + RitariHitPoints);
-                Console.WriteLine("Örkki HP: " + ÖrkkiHitPoints);
-                Console.WriteLine("Valitse toiminto:");
-                Console.WriteLine("1 - Hyökkää");
-                Console.WriteLine("2 - Puolusta");
-
-                string vastaus = "";
-
-                // Kysy komentoa kunnes se on oikein
-                while (vastaus != "1" && vastaus != "2")
-                {
-                    Console.Write("Komento: ");
-                    vastaus = Console.ReadLine();
-                }
-
-                // Pelaajan vuoro
-                if (vastaus == "1")
-                {
-                    int ritariVahinko = random.Next(1, 6);
-                    ÖrkkiHitPoints -= ritariVahinko;
-                    Console.WriteLine("Hyökkäät ja teet örkkiin " + ritariVahinko + " vahinkoa.");
-                }
-                else if (vastaus == "2")
-                {
-                    Console.WriteLine("Puolustaudut kilvellä.");
-                }
-
-                // Örkin vuoro
-                if (ÖrkkiHitPoints > 0)
-                {
-                    int örkkiVahinko = random.Next(1, 6);
-
-                    if (vastaus == "2")
-                    {
-                        örkkiVahinko = örkkiVahinko / 2;
-                    }
-
-                    RitariHitPoints -= örkkiVahinko;
-                    Console.WriteLine("Örkki hyökkää ja tekee sinuun " + örkkiVahinko + " vahinkoa.");
-                }
-
-                Console.WriteLine();
-            }
-
-            if (RitariHitPoints > 0)
-            {
-                Console.WriteLine("Voitit taistelun! Örkki on kukistettu.");
-            }
-            else
-            {
-                Console.WriteLine("Hävisit taistelun. Örkki voitti.");
-            }
-
-            Console.ReadKey();
+            Tulosta(ritari, örkki);
+            string toiminto = PelaajaValinta();
+            örkki = PelaajanVuoro(toiminto, örkki);
+            if (örkki > 0) ritari = ÖrkinVuoro(toiminto, ritari);
+            Console.WriteLine();
         }
+
+        if (ritari > 0) Console.WriteLine("Voitit taistelun! Örkki on kukistettu.");
+        else Console.WriteLine("Hävisit taistelun. Örkki voitti.");
+        Console.ReadKey();
+    }
+
+    static void Tulosta(int ritari, int örkki)
+    {
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("Ritari HP: " + ritari);
+        Console.WriteLine("Örkki HP: " + örkki);
+        Console.WriteLine("Valitse toiminto:");
+        Console.WriteLine("1 - Hyökkää");
+        Console.WriteLine("2 - Puolusta");
+    }
+
+    static string PelaajaValinta()
+    {
+        string valinta = "";
+        while (valinta != "1" && valinta != "2")
+        {
+            Console.Write("Komento: ");
+            valinta = Console.ReadLine();
+        }
+        return valinta;
+    }
+
+    static int PelaajanVuoro(string toiminto, int örkki)
+    {
+        if (toiminto == "1")
+        {
+            int vahinko = random.Next(1, 6);
+            örkki -= vahinko;
+            Console.WriteLine("Hyökkäät ja teet örkkiin " + vahinko + " vahinkoa.");
+        }
+        else
+        {
+            Console.WriteLine("Puolustaudut kilvellä.");
+        }
+        return örkki;
+    }
+
+    static int ÖrkinVuoro(string pelaaja, int ritari)
+    {
+        int vahinko = random.Next(1, 6);
+        if (pelaaja == "2") vahinko /= 2;
+        ritari -= vahinko;
+        Console.WriteLine("Örkki hyökkää ja tekee sinuun " + vahinko + " vahinkoa.");
+        return ritari;
     }
 }
---
+++ Ruoka annos generaattori/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Ruoka_annos_generaattori
+{
+    // Enumeraattorit
+    enum PaaraakaAine { nautaa, kanaa, kasviksia }
+    enum Lisuke { perunaa, riisiä, pastaa }
+    enum Kastike { pippuri, chili, tomaatti, curry }
+
+    // Ateria-luokka
+    class Ateria
+    {
+        public PaaraakaAine PaaraakaAine;
+        public Lisuke Lisuke;
+        public Kastike Kastike;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Pääraaka-aine (nautaa, kanaa, kasviksia): ");
+            PaaraakaAine paaraakaAine =
+                (PaaraakaAine)Enum.Parse(typeof(PaaraakaAine), Console.ReadLine());
+
+            Console.Write("Lisukkeet (perunaa, riisiä, pastaa): ");
+            Lisuke lisuke =
+                (Lisuke)Enum.Parse(typeof(Lisuke), Console.ReadLine());
+
+            Console.Write("Kastike (pippuri, chili, tomaatti, curry): ");
+            Kastike kastike =
+                (Kastike)Enum.Parse(typeof(Kastike), Console.ReadLine());
+
+            Ateria ateria = new Ateria
+            {
+                PaaraakaAine = paaraakaAine,
+                Lisuke = lisuke,
+                Kastike = kastike
+            };
+
+            Console.WriteLine();
+            Console.WriteLine($"{ateria.PaaraakaAine} ja {ateria.Lisuke} {ateria.Kastike}-kastikkeella");
+        }
+    }
+}
