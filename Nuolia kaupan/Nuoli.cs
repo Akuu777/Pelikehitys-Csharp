2e56a1c980eb4011fd48bf350d2a656c20edbf23
akuuu <s2401258@edu.bc.fi>
2026-03-05 11:54:31 +0200

    nuolia kaupan 3.3

---
+++ Nuolia kaupan/Nuoli.cs
@@ -0,0 +1,39 @@
+﻿namespace Nuolia_kaupan
+{
+    class Nuoli
+    {
+        public Karki karki { get; }
+        public Pera pera { get; }
+        public int pituus { get; }
+
+        public Nuoli(Karki k, Pera p, int pit)
+        {
+            karki = k;
+            pera = p;
+            pituus = pit;
+        }
+
+        public double PalautaHinta()
+        {
+            double hinta = 0;
+
+            if (karki == Karki.Puu)
+                hinta += 3;
+            else if (karki == Karki.Teräs)
+                hinta += 5;
+            else if (karki == Karki.Timantti)
+                hinta += 50;
+
+            if (pera == Pera.Lehti)
+                hinta += 0;
+            else if (pera == Pera.Kanansulka)
+                hinta += 1;
+            else if (pera == Pera.Kotkansulka)
+                hinta += 5;
+
+            hinta += pituus * 0.05;
+
+            return hinta;
+        }
+    }
+}
--- Nuolia kaupan/Program.cs
+++ Nuolia kaupan/Program.cs
@@ -14,57 +14,6 @@
         Kotkansulka
     }
 
-    class Nuoli
-    {
-        private Karki karki;
-        private Pera pera;
-        private int pituus;
-
-        public Nuoli(Karki k, Pera p, int pit)
-        {
-            karki = k;
-            pera = p;
-            pituus = pit;
-        }
-        public Karki GetKarki()
-        {
-            return karki;
-        }
-
-        public Pera GetPera()
-        {
-            return pera;
-        }
-
-        public int GetPituus()
-        {
-            return pituus;
-        }
-
-        public double PalautaHinta()
-        {
-            double hinta = 0;
-
-            if (karki == Karki.Puu)
-                hinta += 3;
-            else if (karki == Karki.Teräs)
-                hinta += 5;
-            else if (karki == Karki.Timantti)
-                hinta += 50;
-
-            if (pera == Pera.Lehti)
-                hinta += 0;
-            else if (pera == Pera.Kanansulka)
-                hinta += 1;
-            else if (pera == Pera.Kotkansulka)
-                hinta += 5;
-
-            hinta += pituus * 0.05;
-
-            return hinta;
-        }
-    }
-
     class Program
     {
         static void Main()
