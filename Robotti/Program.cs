9615f09fc94f5a4637cdc982ccc9915a0733654e
akuuu <s2401258@edu.bc.fi>
2026-04-13 12:01:45 +0300

    robotti tehtävä

---
+++ Robotti/Program.cs
@@ -0,0 +1,79 @@
+﻿public class Robotti
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool OnKäynnissä { get; set; }
+    public RobottiKäsky?[] Käskyt { get; } = new RobottiKäsky?[3];
+
+    public void Suorita()
+    {
+        foreach (RobottiKäsky? käsky in Käskyt)
+        {
+            käsky?.Suorita(this);
+            Console.WriteLine($"Robotti: [{X} {Y} {OnKäynnissä}]");
+        }
+    }
+}
+
+public abstract class RobottiKäsky
+{
+    public abstract void Suorita(Robotti robotti);
+}
+
+public class Käynnistä : RobottiKäsky
+{
+    public override void Suorita(Robotti robotti) => robotti.OnKäynnissä = true;
+}
+
+public class Sammuta : RobottiKäsky
+{
+    public override void Suorita(Robotti robotti) => robotti.OnKäynnissä = false;
+}
+
+public class YlösKäsky : RobottiKäsky
+{
+    public override void Suorita(Robotti robotti) { if (robotti.OnKäynnissä) robotti.Y++; }
+}
+
+public class AlasKäsky : RobottiKäsky
+{
+    public override void Suorita(Robotti robotti) { if (robotti.OnKäynnissä) robotti.Y--; }
+}
+
+public class VasenKäsky : RobottiKäsky
+{
+    public override void Suorita(Robotti robotti) { if (robotti.OnKäynnissä) robotti.X--; }
+}
+
+public class OikeaKäsky : RobottiKäsky
+{
+    public override void Suorita(Robotti robotti) { if (robotti.OnKäynnissä) robotti.X++; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Robotti robotti = new Robotti();
+
+        for (int i = 0; i < robotti.Käskyt.Length; i++)
+        {
+            Console.Write("Mitä komentoja syötetään robotille? Vaihtoehdot: Käynnistä, Sammuta, Ylös, Alas, Oikea, Vasen.\n");
+            string? syöte = Console.ReadLine()?.Trim();
+
+            robotti.Käskyt[i] = syöte switch
+            {
+                "Käynnistä" => new Käynnistä(),
+                "Sammuta" => new Sammuta(),
+                "Ylös" => new YlösKäsky(),
+                "Alas" => new AlasKäsky(),
+                "Vasen" => new VasenKäsky(),
+                "Oikea" => new OikeaKäsky(),
+                _ => null
+            };
+        }
+
+        Console.WriteLine();
+        robotti.Suorita();
+    }
+}
